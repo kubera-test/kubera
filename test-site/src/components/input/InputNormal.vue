2ea3f94be6e4b9c17c5814a6841711bc4ba3a7e5
dyamada-jaki <biz.daisuke.yamada@gmail.com>
2022-07-30 17:17:04 +0900

    テスト用のサイトの作成

---
+++ test-site/src/components/input/InputNormal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <b-card>
+      <b-row>
+        <b-col sm="3">
+          <label>Type:Text</label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-input id="idInputText" name="nameInputText" type="text"></b-form-input>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
---
+++ test-site/src/views/InputView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <InputNormal />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InputNormal from '@/components/input/InputNormal.vue'
+
+export default {
+  name: 'InputView',
+  components: {
+    InputNormal
+  }
+}
+</script>
