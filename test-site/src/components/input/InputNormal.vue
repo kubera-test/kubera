680adeedb79852fafc05507cc2ee4dbfc32a9837
dyamada-jaki <biz.daisuke.yamada@gmail.com>
2022-08-01 12:10:32 +0900

    チェックボックステスト用の項目を追加

--- test-site/src/components/input/InputNormal.vue
+++ test-site/src/components/input/InputNormal.vue
@@ -19,12 +19,28 @@
           <b-form-radio id="idInputRadioC" name="nameInputRadio" class="classInputRadio" value="C">Option C</b-form-radio>
         </b-col>
       </b-row>
+      <b-row>
+        <b-col sm="3">
+          <label>Type:CheckBox</label>
+        </b-col>
+        <b-col sm="9">
+          <div class="classInputCheckGroup">
+            <b-form-checkbox id="idInputCheckA" name="nameInputCheckA" class="classInputCheck" value="A">Check A</b-form-checkbox>
+            <b-form-checkbox id="idInputCheckB" name="nameInputCheckB" class="classInputCheck" value="B">Check B</b-form-checkbox>
+            <b-form-checkbox id="idInputCheckC" name="nameInputCheckC" class="classInputCheck" value="C">Check C</b-form-checkbox>
+          </div>
+        </b-col>
+      </b-row>
     </b-card>
   </div>
 </template>
 
 <style>
 div.classInputRadio {
+  text-align: left;
+}
+
+div.classInputCheckGroup {
   text-align: left;
 }
 </style>
