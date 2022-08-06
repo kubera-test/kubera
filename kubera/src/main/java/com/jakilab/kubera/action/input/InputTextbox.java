68365f49b0e8eeda3eb853af2fd7bf9ec1bbbfc3
dyamada-jaki <biz.daisuke.yamada@gmail.com>
2022-08-06 13:39:01 +0900

    テキストボックスの配列要素に対応

--- kubera/src/main/java/com/jakilab/kubera/action/input/InputTextbox.java
+++ kubera/src/main/java/com/jakilab/kubera/action/input/InputTextbox.java
@@ -9,6 +9,7 @@ public class InputTextbox implements Action {
     private String locator;
     private String searchExpression;
     private String inputString;
+    private int index = 0;
 
     public String getLocator() {
         return locator;
@@ -34,8 +35,20 @@ public class InputTextbox implements Action {
         this.inputString = inputString;
     }
 
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
     @Override
     public void execute() {
-        Selenide.$(LocateGenerator.getInstance().getLocator(locator, searchExpression)).setValue(inputString);
+        if (index == 0) {
+            Selenide.$(LocateGenerator.getInstance().getLocator(locator, searchExpression)).setValue(inputString);
+        } else {
+            Selenide.$$(LocateGenerator.getInstance().getLocator(locator, searchExpression)).get(index - 1).setValue(inputString);
+        }
     }
 }
---
+++ kubera/src/test/java/com/jakilab/kubera/action/InputTextArrayTest.java
@@ -0,0 +1,39 @@
+package com.jakilab.kubera.action;
+
+import com.jakilab.kubera.Kubera;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InputTextArrayTest {
+    private Kubera kubera;
+
+    @BeforeEach
+    public void setUp() {
+        kubera = new Kubera();
+    }
+
+    @Test
+    public void 配列項目のテキストボックスに対してIDとインデックスを指定して入力と検証ができること() {
+
+        kubera.action("{ \"actionName\": \"gotoURL\", "
+                + "\"actionJson\": { \"url\": \"http://localhost:8080/input\" } }");
+
+        kubera.action("{ \"actionName\": \"assertTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"checkValue\": \"\", \"index\": 1 } }");
+        kubera.action("{ \"actionName\": \"assertTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"checkValue\": \"\", \"index\": 2 } }");
+        kubera.action("{ \"actionName\": \"assertTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"checkValue\": \"\", \"index\": 3 } }");
+
+        kubera.action("{ \"actionName\": \"inputTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"inputString\": \"idを入力\", \"index\": 2 } }");
+        kubera.action("{ \"actionName\": \"assertTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"checkValue\": \"idを入力\", \"index\": 2 } }");
+
+        kubera.action("{ \"actionName\": \"inputTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"inputString\": \"\", \"index\": 2 } }");
+        kubera.action("{ \"actionName\": \"assertTextbox\", "
+                + "\"actionJson\": { \"locator\": \"id\", \"searchExpression\": \"idInputTextArray\", \"checkValue\": \"\", \"index\": 2 } }");
+    }
+
+}
