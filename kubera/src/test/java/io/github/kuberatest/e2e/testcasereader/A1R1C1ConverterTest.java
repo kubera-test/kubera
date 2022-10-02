a02b753647ca56cafb133596b7862023c209cfe3
dyamada-jaki <edy.jaki@gmail.com>
2022-10-02 23:15:22 +0900

    テストがパスできるように修正

--- kubera/src/test/java/io/github/kuberatest/e2e/testcasereader/A1R1C1ConverterTest.java
+++ kubera/src/test/java/io/github/kuberatest/e2e/testcasereader/A1R1C1ConverterTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class A1R1C1ConverterTest {
     @Test
     public void column() {
-        assertEquals(1, A1R1C1Converter.column("A1"));
-        assertEquals(27, A1R1C1Converter.column("AA1"));
+        assertEquals(0, A1R1C1Converter.column("A1"));
+        assertEquals(26, A1R1C1Converter.column("AA1"));
     }
 
     @Test
     public void row() {
-        assertEquals(1, A1R1C1Converter.row("A1"));
-        assertEquals(11, A1R1C1Converter.row("AB11"));
+        assertEquals(0, A1R1C1Converter.row("A1"));
+        assertEquals(10, A1R1C1Converter.row("AB11"));
     }
 }
